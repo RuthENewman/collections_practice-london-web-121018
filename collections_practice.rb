c508cf0f075e180dc1739f956dd94dfd8bb42fd2
ruthnewman <ruthnewman@gmail.com>
2018-12-11 14:50:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  new_array = array.sort
+  new_array
+end 
 
+def sort_array_desc(array)
+  new_array = array.sort { |x, y| x > y }
+  new_array
+end 
+
+def sort_array_char_count(array)
+  new_array = array.sort { |x, y| x.length < y.length }
+  new_array
+end 
+
+def swap_elements(array)
+  
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+end 
+
+def find_a(array)
+end 
+
+def sum_array(array)
+  total = 0 
+  array.each do |element|
+    if element == Integer
+      total += element 
+    end 
+  end
+  total
+end
+
+def add_s(array)
+  array.map do |word|
+    if word == array[1]
+      word 
+    else 
+      word
+  end 
+end 
+    
